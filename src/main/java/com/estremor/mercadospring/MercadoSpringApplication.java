afbdc605e0ce3714a776483e0cdf0d0d73f59994
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2023-03-05 21:59:36 -0500

    Hola mundo con archivos de configuracion

---
+++ src/main/java/com/estremor/mercadospring/MercadoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.estremor.mercadospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estremor/mercadospring/MercadoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estremor.mercadospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
