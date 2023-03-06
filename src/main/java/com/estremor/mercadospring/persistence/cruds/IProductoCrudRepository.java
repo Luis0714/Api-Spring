22853c10d6ab51180a5661e2cdc2578ac5dba10b
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2023-03-05 23:58:09 -0500

    Creacion del repository de Producto

--- src/main/java/com/estremor/mercadospring/persistence/cruds/IProductoCrudRepository.java
+++ src/main/java/com/estremor/mercadospring/persistence/cruds/IProductoCrudRepository.java
@@ -1,7 +1,15 @@
 package com.estremor.mercadospring.persistence.cruds;
 
 import com.estremor.mercadospring.persistence.entities.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IProductoCrudRepository extends CrudRepository<Producto,Integer> {
+
+   // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true) // forma nativa
+   List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+   Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
