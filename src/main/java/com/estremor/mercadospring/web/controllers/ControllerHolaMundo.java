29f6aa3d279a6bf84939d6a83b026d80d0b8ebbe
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2023-03-05 22:05:46 -0500

    Creacion de la estructura por capas

--- src/main/java/com/estremor/mercadospring/web/controllers/ControllerHolaMundo.java
+++ src/main/java/com/estremor/mercadospring/web/controllers/ControllerHolaMundo.java
@@ -1,4 +1,4 @@
-package com.estremor.mercadospring;
+package com.estremor.mercadospring.web.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
